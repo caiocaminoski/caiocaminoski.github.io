69160e0283c5b78688ada9203040fe694efb9bee
Caio Engelhardt <caio_caminoski@hotmail.com>
2024-07-16 18:38:14 +0200

    added config for test

---
+++ street-gender-equality/config/curitiba.js
@@ -0,0 +1,16 @@
+// ********** CITY VARIABLES ***************
+var cityName = 'Curitiba';
+// var csvUrl = 'https://raw.githubusercontent.com/caiocaminoski/caiocaminoski.github.io/main/street-gender-equality/data/curitiba.csv';
+
+// defining viewbox
+var minLat = -25.6489423;
+var maxLat = -25.343117;
+var minLon = -49.3926966;
+var maxLon = -49.1832167;
+
+var scalingFactor = 0.5; // 0 = original
+
+var speedPlacement = 1;
+
+var compensationXDesktop = 160;  // for desktop view it may be needed to add a compensation if scaling factor is set to >0
+var compensationYMobile = 40; // for mobile view it may be needed to add a compensation if scaling factor is set to >0
