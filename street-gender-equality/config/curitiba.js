8c430f482570320ea983c62da9ce19b65f14668f
Caio Engelhardt <caio_caminoski@hotmail.com>
2024-07-19 15:54:16 +0200

    added english support and sao paulo dummy data for testing

--- street-gender-equality/config/curitiba.js
+++ street-gender-equality/config/curitiba.js
@@ -1,6 +1,5 @@
 // ********** CITY VARIABLES ***************
 var cityName = 'Curitiba';
-// var csvUrl = 'https://raw.githubusercontent.com/caiocaminoski/caiocaminoski.github.io/main/street-gender-equality/data/curitiba.csv';
 
 // defining viewbox
 var minLat = -25.6489423;
@@ -9,8 +8,7 @@ var minLon = -49.3926966;
 var maxLon = -49.1832167;
 
 var scalingFactor = 0.5; // 0 = original
-
-var speedPlacement = 1;
-
 var compensationXDesktop = 160;  // for desktop view it may be needed to add a compensation if scaling factor is set to >0
 var compensationYMobile = 40; // for mobile view it may be needed to add a compensation if scaling factor is set to >0
+
+var speedPlacement = 1;
---
+++ street-gender-equality/config/sao-paulo.js
@@ -0,0 +1,14 @@
+// ********** CITY VARIABLES ***************
+var cityName = 'São Paulo';
+
+// defining viewbox
+const minLat = -23.9623094
+const maxLat = -23.358101
+const minLon = -46.8258755
+const maxLon = -46.36518
+
+var scalingFactor = 0.7; // 0 = original
+var compensationXDesktop = 260;  // for desktop view it may be needed to add a compensation if scaling factor is set to >0
+var compensationYMobile = 40; // for mobile view it may be needed to add a compensation if scaling factor is set to >0
+
+var speedPlacement = 3;
