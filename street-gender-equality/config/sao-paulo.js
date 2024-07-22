4f9f6f39a80c22e62bdc2a60fd56ed730617b38e
Caio Engelhardt <caio_caminoski@hotmail.com>
2024-07-22 18:51:59 +0200

    added google form first version

--- street-gender-equality/config/sao-paulo.js
+++ street-gender-equality/config/sao-paulo.js
@@ -11,4 +11,6 @@ var scalingFactor = 0.7; // 0 = original
 var compensationXDesktop = 260;  // for desktop view it may be needed to add a compensation if scaling factor is set to >0
 var compensationYMobile = 40; // for mobile view it may be needed to add a compensation if scaling factor is set to >0
 
-var speedPlacement = 3;
+var speedPlacement = 30;
+
+var animationEnabled = 0;
