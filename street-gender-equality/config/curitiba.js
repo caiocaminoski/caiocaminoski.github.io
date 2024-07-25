aec4d96096b00bd753a774c050288b1b64bdadfa
Caio Engelhardt <caio_caminoski@hotmail.com>
2024-07-25 18:23:47 +0200

    adjusments to contextmenu | form max width | csv data curitiba

--- street-gender-equality/config/curitiba.js
+++ street-gender-equality/config/curitiba.js
@@ -7,7 +7,8 @@ var maxLat = -25.343117;
 var minLon = -49.3926966;
 var maxLon = -49.1832167;
 
-var scalingFactor = 0.6; // 0 = original
+var scalingFactor_mobile = 0.6; // 0 = original
+var scalingFactor_desktop = 0.3; // 0 = original
 var compensationXDesktop = 160;  // for desktop view it may be needed to add a compensation if scaling factor is set to >0
 var compensationYMobile = 40; // for mobile view it may be needed to add a compensation if scaling factor is set to >0
 
